64d8f086bdb7b98945f137d0c083589fc5275916
giulyquinto <onlyforwin@gmail.com>
2014-03-16 15:55:13 +0100

    added redirect handler which will check for search parameters in the url and if access_token is present it will save it into localStorage

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,14 @@
 export default Ember.Route.extend({
   model: function() {
     return ['red', 'yellow', 'blue'];
+  },
+  redirect: function() {
+    var hash = window.location.search;
+    if (hash !== "") {
+      var pars = hash.split('&');
+      var tmp = pars[0].split("=")
+      if (tmp[0] == "?access_token")
+        localStorage.setItem("access_token", tmp[1]);
+    }
   }
 });
