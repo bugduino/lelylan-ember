8c7351eb7e082d448d8f3323ea278eb3fa99fd42
giulyquinto <onlyforwin@gmail.com>
2014-03-16 19:14:47 +0100

    naming fixed

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,10 +3,10 @@ export default Ember.Route.extend({
     return ['red', 'yellow', 'blue'];
   },
   redirect: function() {
-    var hash = window.location.search;
-    if (hash !== "") {
-      var pars = hash.split('&');
-      var tmp = pars[0].split("=");
+    var search = window.location.search;
+    if (search !== "") {
+      var params = search.split('&');
+      var tmp = params[0].split("=");
       if (tmp[0] === "?access_token")
         localStorage.setItem("access_token", tmp[1]);
     }
