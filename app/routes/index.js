d9d80f93bfd80d7a9523afc6dac7d844cdcde8c7
giulyquinto <onlyforwin@gmail.com>
2014-03-17 21:04:04 +0100

    added devices resource

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,10 +5,11 @@ export default Ember.Route.extend({
   redirect: function() {
     var search = window.location.search;
     if (search !== "") {
-      var params = search.split('&');
-      var tmp = params[0].split("=");
-      if (tmp[0] === "?access_token")
-        localStorage.setItem("access_token", tmp[1]);
+      var params = search.split('&')[0].split("=");
+      if (params[0].split('?')[1] === "access_token")
+        localStorage.setItem("access_token", params[1]);
+        //TODO do not refresh the page but removing query params from url
+        window.location = window.location.origin + "/#/devices";
     }
   }
 });
