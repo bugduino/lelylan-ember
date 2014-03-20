ef57f1ea3dff93cd305e2d663899ab61bdb790f4
William Bergamo <onlyforwin@gmail.com>
2014-03-20 15:07:30 +0100

    Update oauth2.js

--- app/oauth2.js
+++ app/oauth2.js
@@ -1,9 +1,9 @@
 export default Ember.OAuth2.config = {
   lelylan: {
-    clientId: "e466b1c8157d1a7f459304a3634ec4a2db35c824b69727632c3a97e6c4bc2bf6",
+    clientId: "xxx",
     authBaseUri: 'http://people.lelylan.com/oauth/authorize',
     redirectUri: 'http://localhost:8000/vendor/callback',
-    client_secret: "95e597d208d610b4fc72696fbe4b552dab61e1f8ca0113da1ed7cc3df7f474ee",
+    client_secret: "xxx",
     scope: 'devices',
     statePrefix: "lelylan"
   } 
