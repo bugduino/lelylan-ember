81cdb7b62ed34a3764f55bf3eb5d276907d6b4c9
giulyquinto <onlyforwin@gmail.com>
2014-03-24 13:05:04 +0100

    added device model route and minimal template

--- app/models/device.js
+++ app/models/device.js
@@ -1,3 +1,14 @@
+var attr = DS.attr;
+
 export default DS.Model.extend({
-  
+  uri: attr(),
+  id: attr(),
+  name: attr(),
+  type: attr(),
+  physical: attr(),
+  owner: attr(),
+  maker: attr(),
+  activated: attr(),
+  categories: attr(),
+  properties: attr()
 });
--- app/routes/devices.js
+++ app/routes/devices.js
@@ -1,2 +1,19 @@
 export default Ember.Route.extend({
+  
+  model: function() {
+    var url = "http://api.lelylan.com/devices.json";
+    var header = "Bearer " + localStorage.getItem('access_token');
+  
+    Em.$.ajax(url, {
+      headers: {"Authorization": header},
+      success: function(res) {
+        return res;
+      },
+      error: function(err) {
+        console.log('error ' + err);
+      }
+    });
+  
+  }
+
 });
