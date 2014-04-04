955882df4352f270e86ea6b47d8d152d2471f73a
giulyquinto <onlyforwin@gmail.com>
2014-04-04 14:16:50 +0200

    added application serializer

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,2 +1,7 @@
 export default DS.RESTSerializer.extend({
+  extractArray: function(store, type, payload, id, requestType) {
+    var devices = { "devices": payload };
+    debugger;
+    return this._super(store, type, devices, id, requestType);
+  }
 });
